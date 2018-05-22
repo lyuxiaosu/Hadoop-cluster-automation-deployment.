f6e7a24f622565bc129029219ec218d7a774c593
lyuxiaosu <lyuxiaosu@email.gwu.edu>
2018-05-22 13:48:12 -0400

    add two new perl script

---
+++ auto_deploy_diff_hardware/create_containers.pl
@@ -0,0 +1,117 @@
+#! /usr/bin/perl
+
+use List::Util qw[min max];
+
+sub get_cpu_period_quota {
+	my $cpu_share = $_[0];
+        my @cpu_period_quota;
+        if ($cpu_share == 0.2) {
+                @cpu_period_quota=(50000, 10000);
+        }
+        elsif ($cpu_share == 0.4) {
+                @cpu_period_quota=(50000, 20000);
+        }
+        elsif ($cpu_share == 0.6) {
+                @cpu_period_quota=(50000, 30000);
+        }
+        elsif ($cpu_share == 0.8) {
+                @cpu_period_quota=(50000, 40000);
+        }
+        elsif ($cpu_share == 1) {
+                @cpu_period_quota=(50000, 50000);
+        }
+        elsif ($cpu_share == -1) {
+                @cpu_period_quota=(-1, -1);
+        }
+        else {
+                @cpu_period_quota=(-1, -1);
+                print ("invalid cpu_share, no match\n");
+        }
+	return @cpu_period_quota;
+}
+
+$max_memory = 32;
+$current_memory = 0;
+$max_cpu_core = 4;
+%cpucore_used=(0, 0, 1, 0, 2, 0, 3, 0);
+sub get_cpusets {
+	$cpu_core = $_[0];
+	$cpu_share = $_[1];
+	$memory = $_[2];
+	if ($cpu_share == -1) {
+		$each_cpu_core_share = 1;
+	} else {
+		$each_cpu_core_share = $cpu_share/$cpu_core;
+	}
+	$i = 0;
+	@cpusets = ();
+	$current_memory = $current_memory + $memory;
+	if ($current_memory > $max_memory) {
+		print("exceed the max memory, return empty cpusets\n");
+		return @cpusets;
+	}
+
+	while ($i < $cpu_core) {# cpu_core is the needed cpu number
+		$j = 0;
+		for (; $j < $max_cpu_core; $j++) {
+			if ($cpucore_used{$j} + $each_cpu_core_share <= 1 && !($j ~~ @cpusets)) {
+				$cpucore_used{$j} = $cpucore_used{$j} + $each_cpu_core_share; 
+				push(@cpusets, $j);
+				last;
+			}
+		}
+		if ($j == $max_cpu_core) {
+			last;
+		} else {
+			$i = $i + 1;
+		}
+	} 
+	$array_len = @cpusets;
+	if ($array_len != $cpu_core) {
+		@cpusets = ();
+	}
+	return @cpusets;
+}
+
+sub reset_resource {
+	$current_memory = 0;
+	%cpucore_used=(0, 0, 1, 0, 2, 0, 3, 0);
+}
+
+$image_name=$_[0];
+
+open (f, "< hardware_configure2.txt") or die "Open hardware_configure2.txt fail, $!";
+readline f; #skip the frist line
+
+print("$count[0]\t$count[1]\n");
+$index = 2;
+$container_index = 1;
+while ($line=<f>) {
+	@array = split("\t", $line);
+	$cpu_core = $array[0];
+	$cpu_share = $array[1];
+	$memory = $array[2];
+	$disk_speed = $array[3];
+	@cpu_period_quota = get_cpu_period_quota($cpu_share);
+	@cpusets = get_cpusets($cpu_core, $cpu_share, $memory);
+	$array_len = @cpusets;
+	if ($array_len == 0) {
+		print("reach the maximum containers allocation, line_index=$index, cpu_core:$cpu_core cpu_share:$cpu_share memory:$memory\n");
+		#the cluster has been finished, run applications one by one
+
+		#after running, destroy the cluster and reset the resource
+		reset_resource();
+		$container_index = 1;
+	} else {
+		print("container=$container_index,line_index=$index:cpusets= @cpusets cpu_core:$cpu_core cpu_share:$cpu_share memory:$memory\n");
+		$str_cpusets = join(',', @cpusets);
+		print ("str_cpusets:$str_cpusets\n");
+		#print join(',', @cpusets);
+		#create a new container
+		$index++;
+		$container_index++;
+	}
+
+}
+
+close f;
---
+++ auto_deploy_diff_hardware/generate_hardware_config.pl
@@ -0,0 +1,39 @@
+#! /usr/bin/perl
+
+#@cpu_core = (1, 2, 3, 4);
+@cpu_core = (1, 2, 4);
+#@cpu_share = (1/5, 2/5, 3/5, 4/5, 1, -1);
+@cpu_share = (1/5, 3/5, 1, -1);
+#@memory = (1, 2, 4, 8, 16, 32);
+@memory = (2, 4, 8, 16, 32);
+#@disk_read_speed = (10, 20, 40, 60, 80, 100); 
+@disk_read_speed = (10, 30, 60, 100);
+
+open(f, "> hardware_configure.txt") or die "Open hardware_configure.txt fail, $!";
+open(f2, "> hardware_configure2.txt") or die "Open hardware_configure2.txt fail, $!";
+
+print f "cpu_core\tcpu_frequency\tmemory\tdisk_read_speed\tdisk_write_speed\n";
+print f2 "cpu_core\tcpu_share\tmemory\tdisk_read_speed\tdisk_write_speed\n";
+
+$r = `lscpu |grep MHz`;
+@array = split(/\s+/, $r);
+
+foreach $cc (@cpu_core) {
+	foreach $cs (@cpu_share) {
+		foreach $m (@memory) {
+			foreach $drs (@disk_read_speed) {
+				local $cpu_freqency = ($array[2] * $cs)/$cc;
+				if ($cs == -1 && $cc == 1) {
+					next;
+				} elsif ($cs == -1 && $cc != 1) {
+					$cpu_freqency = $array[2];
+				}
+				print f "$cc\t$cpu_freqency\t$m\t$drs\t$drs\n";				
+				print f2 "$cc\t$cs\t$m\t$drs\t$drs\n";				
+			}
+		}
+	}
+}
+ 
+close f;
+close f2;
