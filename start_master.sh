900657ac4fc1f38246c8bbe71a5fb08897290e07
lyuxiaosu <lyuxiaosu@email.gwu.edu>
2018-05-23 13:56:45 -0400

    modified auto_deploy_diff_hardware/create_containers.pl and start_master.sh

--- start_master.sh
+++ start_master.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-docker run -d --name master -h master -m 4G ubuntu:test /root/run.sh 2 1
+
+print_help() {
+	cat <<EOF
+		use $0 docker_image
+EOF
+}
+
+if [ $# != 1 ]; then
+print_help
+exit
+fi
+
+docker run -d --name master -h master -m 2G $1 /root/run.sh 2 1
 
 docker exec -d master /root/start_master.sh
 
